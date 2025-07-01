38279a1f34a30f5fae43d7d8ca84e4c39b6d17b8
Lucas <lucasperuchena.27@gmail.com>
2025-07-01 21:58:11 +0200

    chore: initialize Spring Boot project with base dependencies

---
+++ src/test/java/com/kairos_assignment/lucas/LucasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kairos_assignment.lucas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LucasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
