2cc9c4cc0cf7e370d4111a1ca3aea97af5a8be43
Lucas <lucasperuchena.27@gmail.com>
2025-07-02 08:19:23 +0200

    feat: mapper implementation modification
    - adapt method to add productID and brandID to responseDTO

--- src/main/java/com/kairos_assignment/lucas/application/mapper/PriceMapper.java
+++ src/main/java/com/kairos_assignment/lucas/application/mapper/PriceMapper.java
@@ -3,7 +3,12 @@ package com.kairos_assignment.lucas.application.mapper;
 import com.kairos_assignment.lucas.application.dto.PriceResponseDTO;
 import com.kairos_assignment.lucas.domain.entity.Price;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface PriceMapper extends EntityMapper<PriceResponseDTO, Price> {
+
+    @Mapping(target = "productId", source = "product.id")
+    @Mapping(target = "brandId", source = "brand.id")
+    PriceResponseDTO toDto(Price price);
 }
