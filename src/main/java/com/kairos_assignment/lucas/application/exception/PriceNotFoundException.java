030a18e2b39db3978f04b7618a275b5045c6ac13
Lucas <lucasperuchena.27@gmail.com>
2025-07-02 08:20:01 +0200

    feat: implementation of mapper and service
    - service added to resource
    - mapper added to service

---
+++ src/main/java/com/kairos_assignment/lucas/application/exception/PriceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.kairos_assignment.lucas.application.exception;
+
+public class PriceNotFoundException extends RuntimeException {
+
+    public PriceNotFoundException(String message) {
+        super(message);
+    }
+}
